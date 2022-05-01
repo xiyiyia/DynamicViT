360faa65304342fe07d43048e83168cf6f808c6b
xiyiyia <boqianfu@gmail.com>
2022-05-01 19:29:37 +0900

    classfy_image

--- utils/classifydata.py
+++ utils/classifydata.py
@@ -1,10 +1,12 @@
 import os
 
 fo = open("ILSVRC2012_validation_ground_truth.txt", "r")
-
+i = 1
 for label in fo:
     label_len = len(label)
+    index_len = len(str(i))
     os.system('mv /home/ubuntu/datasets/ILSVRC2012_val/ILSVRC2012_val_000'
-              + ''.join(['0' for i in range(5 - label_len)]) + label +
-              '.JPEG')
+              + ''.join(['0' for i in range(5 - index_len)]) + str(i) + '.JPEG'
+              + ' /home/ubuntu/datasets/ILSVRC2012_val/' + label)
+    i += 1
 fo.close()
