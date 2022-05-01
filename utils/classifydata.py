61bd8f8502c7568552bb7a40e3d861bcde56a63c
xiyiyia <boqianfu@gmail.com>
2022-05-01 20:00:52 +0900

    classfy_image

--- utils/classifydata.py
+++ utils/classifydata.py
@@ -5,7 +5,7 @@ i = 1
 for label in fo:
     label_len = len(label)
     index_len = len(str(i))
-    os.system('mv /home/ubuntu/datasets/ILSVRC2012_val/ILSVRC2012_val_000'
+    os.system('mv /home/ubuntu/datasets/ILSVRC2012_val/val/ILSVRC2012_val_000'
               + ''.join(['0' for i in range(5 - index_len)]) + str(i) + '.JPEG'
               + ' /home/ubuntu/datasets/ILSVRC2012_val/val/'+label)
     i += 1
