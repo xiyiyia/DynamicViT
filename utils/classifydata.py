41cecda78ef96b228e2238d66066251d7932af55
xiyiyia <boqianfu@gmail.com>
2022-05-01 19:26:06 +0900

    ground_truth

--- utils/classifydata.py
+++ utils/classifydata.py
@@ -5,6 +5,6 @@ fo = open("ILSVRC2012_validation_ground_truth.txt", "r")
 for label in fo:
     label_len = len(label)
     os.system('mv /home/ubuntu/datasets/ILSVRC2012_val/ILSVRC2012_val_000'
-              + ''.join(['0' for i in range(5 - label_len)]) +
+              + ''.join(['0' for i in range(5 - label_len)]) + label
               '.JPEG')
 fo.close()
