985a6454a239bdbe5b265f4fcadf2dfcc53f35b5
xiyiyia <boqianfu@gmail.com>
2022-05-02 09:31:43 +0900

    classfy_image

--- infer.py
+++ infer.py
@@ -169,7 +169,7 @@ def accuracy(output, target, topk=(1,)):
         _, pred = output.topk(maxk, 1, True, True)
         pred = pred.t()
         # print(pred, target.view(1, -1))
-        time.sleep(5)
+        # time.sleep(5)
         correct = pred.eq(target.view(1, -1).expand_as(pred))
 
         res = []
