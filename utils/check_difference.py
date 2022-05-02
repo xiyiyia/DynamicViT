1d52f738fffa7a60c1f56f75c7cf26ddf503b741
xiyiyia <boqianfu@gmail.com>
2022-05-02 12:36:11 +0900

    classfy_image

--- utils/check_difference.py
+++ utils/check_difference.py
@@ -12,3 +12,16 @@ for i in range(len(five)):
             count += 1
         total += 1
 print(count,total)
+
+f1 = open("../result/correct_index0.6.pickle", "rb")
+five = pickle.load(f1)
+f2 = open("../result/correct_index0.7.pickle", "rb")
+seven = pickle.load(f2)
+count = 0
+total = 0
+for i in range(len(five)):
+    for _, val in enumerate(five[i]):
+        if val not in seven[i]:
+            count += 1
+        total += 1
+print(count,total)
