0d391252398d2c02dd00a396780aa2da2e5ff28c
xiyiyia <boqianfu@gmail.com>
2022-05-02 11:26:06 +0900

    classfy_image

---
+++ utils/check_difference.py
@@ -0,0 +1,8 @@
+import pickle
+
+f1 = open("../result/correct_index0.5.pickle", "rb")
+five = pickle.load(f1)
+f2 = open("../result/correct_index0.7.pickle", "rb")
+seven = pickle.load(f2)
+for i in range(len(five)):
+    print(five not in seven)
