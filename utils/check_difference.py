d720bcfa670afbd77cf26caa300e9167c7171869
xiyiyia <boqianfu@gmail.com>
2022-05-02 13:58:30 +0900

    classfy_image

--- utils/check_difference.py
+++ utils/check_difference.py
@@ -1,24 +1,72 @@
 import pickle
 
+# f1 = open("../result/correct_index0.5.pickle", "rb")
+# five = pickle.load(f1)
+# f2 = open("../result/correct_index0.7.pickle", "rb")
+# seven = pickle.load(f2)
+# count = 0
+# total = 0
+# for i in range(len(five)):
+#     for _, val in enumerate(five[i]):
+#         if val not in seven[i]:
+#             count += 1
+#         total += 1
+# print(count, total)
+# f1.close()
+# f2.close()
+#
+# f1 = open("../result/correct_index0.7.pickle", "rb")
+# five = pickle.load(f1)
+# f2 = open("../result/correct_index0.5.pickle", "rb")
+# seven = pickle.load(f2)
+# count = 0
+# total = 0
+# for i in range(len(five)):
+#     for _, val in enumerate(five[i]):
+#         if val not in seven[i]:
+#             count += 1
+#         total += 1
+# print(count,total)
+# f1.close()
+# f2.close()
+#
+# f1 = open("../result/correct_index0.6.pickle", "rb")
+# five = pickle.load(f1)
+# f2 = open("../result/correct_index0.7.pickle", "rb")
+# seven = pickle.load(f2)
+# count = 0
+# total = 0
+# for i in range(len(five)):
+#     for _, val in enumerate(five[i]):
+#         if val not in seven[i]:
+#             count += 1
+#         total += 1
+# print(count,total)
+# f1.close()
+# f2.close()
+#
+# f1 = open("../result/correct_index0.5.pickle", "rb")
+# five = pickle.load(f1)
+# f2 = open("../result/correct_index0.6.pickle", "rb")
+# seven = pickle.load(f2)
+# count = 0
+# total = 0
+# for i in range(len(five)):
+#     for _, val in enumerate(five[i]):
+#         if val not in seven[i]:
+#             count += 1
+#         total += 1
+# print(count,total)
+# f1.close()
+# f2.close()
+
 f1 = open("../result/correct_index0.5.pickle", "rb")
 five = pickle.load(f1)
 f2 = open("../result/correct_index0.7.pickle", "rb")
 seven = pickle.load(f2)
-count = 0
-total = 0
-for i in range(len(five)):
-    for _, val in enumerate(five[i]):
-        if val not in seven[i]:
-            count += 1
-        total += 1
-print(count,total)
-f1.close()
-f2.close()
+f3 = open("../result/correct_index0.6.pickle", "rb")
+six = pickle.load(f3)
 
-f1 = open("../result/correct_index0.7.pickle", "rb")
-five = pickle.load(f1)
-f2 = open("../result/correct_index0.5.pickle", "rb")
-seven = pickle.load(f2)
 count = 0
 total = 0
 for i in range(len(five)):
@@ -26,36 +74,34 @@ for i in range(len(five)):
         if val not in seven[i]:
             count += 1
         total += 1
-print(count,total)
-f1.close()
-f2.close()
+print(count, total)
 
-f1 = open("../result/correct_index0.6.pickle", "rb")
-five = pickle.load(f1)
-f2 = open("../result/correct_index0.7.pickle", "rb")
-seven = pickle.load(f2)
 count = 0
 total = 0
 for i in range(len(five)):
     for _, val in enumerate(five[i]):
-        if val not in seven[i]:
+        if val not in six[i]:
             count += 1
         total += 1
-print(count,total)
-f1.close()
-f2.close()
+print(count, total)
 
-f1 = open("../result/correct_index0.5.pickle", "rb")
-five = pickle.load(f1)
-f2 = open("../result/correct_index0.6.pickle", "rb")
-seven = pickle.load(f2)
+count = 0
+total = 0
+for i in range(len(six)):
+    for _, val in enumerate(six[i]):
+        if val not in seven[i]:
+            count += 1
+        total += 1
+print(count, total)
+
 count = 0
 total = 0
 for i in range(len(five)):
     for _, val in enumerate(five[i]):
-        if val not in seven[i]:
+        if val in seven[i] and val in six[i]:
             count += 1
         total += 1
-print(count,total)
+print(count, total)
+
 f1.close()
 f2.close()
